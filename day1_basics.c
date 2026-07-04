01e7aefd5fbc790db052897da8526e7fffd7938e
ahmreen <ahmreenkaur7@gmail.com>
2026-07-04 22:22:37 +0530

    Completed C loop practice

--- day1_basics.c
+++ day1_basics.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    int n;
-    printf("Enter number to get sum of first n terms:");
-    scanf("%d",&n);
-    int sum = 0;
-    for(int i = 1;i <= n ;i++){
-        sum += i;
+  for(int i = 1; i<=50;i++){
+    if(i % 3 == 0){
+      printf("%d\n",i);
     }
-    printf("%d is sum of first %d terms.",sum,n);
-    return 0;
+  }
+  return 0;
 }
